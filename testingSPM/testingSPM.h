336fcb63ee443d10f4ed4e24b5ae8f6a005fbb4d
Nilay Avd <nilay@avdevs.com>
2020-12-18 14:17:57 +0530

    Initial Commit

---
+++ testingSPM/testingSPM.h
@@ -0,0 +1,18 @@
+//
+//  testingSPM.h
+//  testingSPM
+//
+//  Created by Nilay Avd on 18/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testingSPM.
+FOUNDATION_EXPORT double testingSPMVersionNumber;
+
+//! Project version string for testingSPM.
+FOUNDATION_EXPORT const unsigned char testingSPMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testingSPM/PublicHeader.h>
+
+
